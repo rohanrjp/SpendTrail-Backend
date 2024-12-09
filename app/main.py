9fb707d4ab889469c92ac2d8213cb1b9ab5de8e7
Rohan <rohan1007rjp@gmail.com>
2024-12-09 16:29:35 +0530

    changes to cors

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app=FastAPI(title="SpendTrail",description="AI based expense tracker",version=ve
 app.include_router(auth_router)
 
 origins=[
-    "https://spendtrail-rg06k5wup-rohan1007rjp-gmailcoms-projects.vercel.app"
+    "spendtrail-2sbnd7a65-rohan1007rjp-gmailcoms-projects.vercel.app"
 ]
 
 app.add_middleware(
