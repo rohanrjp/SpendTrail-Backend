8f30b6b3176b09b7f96dab494b0b12ff7e22aba2
Rohan <rohan1007rjp@gmail.com>
2025-01-24 22:05:21 +0530

    db pooling commit

--- app/core/db.py
+++ app/core/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.core.config import Config
 
-engine=create_engine(Config.DATABASE_URL)
+engine=create_engine(Config.DATABASE_URL,pool_pre_ping=True,pool_size=10,max_overflow=5,pool_recycle=1800)
 Session_Local=sessionmaker(bind=engine,autoflush=False,autocommit=False)
 Base=declarative_base()
 
